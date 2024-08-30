2f24173614522a5389b8b08bcd21cc956cbebc27
“sokyran” <citramonarchy@gmail.com>
2024-08-31 00:19:30 +0300

    try

--- go/go.mod
+++ go/go.mod
@@ -2,9 +2,13 @@ module thermal-printer-handler
 
 go 1.22
 
-require github.com/hennedo/escpos v0.0.1
+require (
+	github.com/hennedo/escpos v0.0.1
+	github.com/seer-robotics/escpos v0.0.0-20200217094910-cfa0c7b89d75
+)
 
 require (
-	github.com/kenshaw/escpos v0.0.0-20221114190919-df06b682a8fc // indirect
+	github.com/astaxie/beego v1.12.3 // indirect
 	github.com/qiniu/iconv v1.2.0 // indirect
+	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
 )
--- go/main.go
+++ go/main.go
@@ -1,56 +1,53 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"os"
 
-	"github.com/kenshaw/escpos"
+	"github.com/seer-robotics/escpos"
 )
 
 func main() {
-	f, err := os.OpenFile("/dev/usb/lp3", os.O_RDWR, 0)
+	// Open the USB device file
+	deviceFile := "/dev/bus/usb/001/002" // Replace with the correct device path
+	file, err := os.OpenFile(deviceFile, os.O_RDWR, 0666)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error opening USB device: %v\n", err)
+		return
 	}
-	defer f.Close()
+	defer file.Close()
 
-	w := bufio.NewWriter(f)
-	p := escpos.New(w)
+	p := escpos.New(file)
+
+	p.Verbose = true
 
 	p.Init()
-	p.SetSmooth(1)
 	p.SetFontSize(2, 3)
 	p.SetFont("A")
-	p.Write("test ")
+	p.Write("test1")
 	p.SetFont("B")
-	p.Write("test2 ")
-	p.SetFont("C")
-	p.Write("test3 ")
-	p.Formfeed()
-
-	p.SetFont("B")
-	p.SetFontSize(1, 1)
+	p.Write("test2")
 
 	p.SetEmphasize(1)
-	p.Write("halle")
+	p.Write("hello")
 	p.Formfeed()
 
 	p.SetUnderline(1)
 	p.SetFontSize(4, 4)
-	p.Write("halle")
+	p.Write("hello")
 
 	p.SetReverse(1)
 	p.SetFontSize(2, 4)
-	p.Write("halle")
-	p.Formfeed()
+	p.Write("hello")
+	p.FormfeedN(10)
 
-	p.SetFont("C")
-	p.SetFontSize(8, 8)
-	p.Write("halle")
-	p.FormfeedN(5)
+	p.SetAlign("center")
+	p.Write("test")
+	p.Linefeed()
+	p.Write("test")
+	p.Linefeed()
+	p.Write("test")
+	p.FormfeedD(200)
 
 	p.Cut()
-	p.End()
-
-	w.Flush()
 }
