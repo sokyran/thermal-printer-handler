1495445a885efae64e59dd04987faa7c0d68f208
“sokyran” <citramonarchy@gmail.com>
2024-08-31 00:41:07 +0300

    try

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const device = new escpos.USB();
 // const device  = new escpos.Network('localhost');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 
-const options = { encoding: "GB18030" }
+const options = { encoding: "utf-8" }
 // encoding is optional
 
 const printer = new escpos.Printer(device, options);
+
+const multilineText = ```
+— А ви заблокували Юру? Він оце без вас поїхав
+— Тоді ще Миру треба…
+— Давайте!
+
+Влад прийшов на коворкинг до Насти та Єви
+```;
+
 
 device.open(function (error) {
   printer
@@ -17,16 +26,10 @@ device.open(function (error) {
     .align('ct')
     .style('bu')
     .size(1, 1)
-    .text('The quick brown fox jumps over the lazy dog')
-    .text('敏捷的棕色狐狸跳过懒狗')
-    .barcode('1234567', 'EAN8')
-    .table(["One", "Two", "Three"])
-    .tableCustom(
-      [
-        { text: "Left", align: "LEFT", width: 0.33, style: 'B' },
-        { text: "Center", align: "CENTER", width: 0.33 },
-        { text: "Right", align: "RIGHT", width: 0.33 }
-      ],
-    )
-    .cut().close();
+
+  multilineText.split('\n').forEach((line) => {
+    printer.text(line);
+  });
+
+  printer.cut().close();
 });
