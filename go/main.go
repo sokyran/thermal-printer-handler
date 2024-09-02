c4f4b382eaeb551b08cb88ecd4d6e875f13bd3bd
“sokyran” <citramonarchy@gmail.com>
2024-09-02 23:18:59 +0300

    try

--- go/main.go
+++ go/main.go
@@ -7,56 +7,64 @@ import (
 	"github.com/google/gousb"
 )
 
+// ESC/POS commands
+var (
+	ESC  = []byte{0x1B}
+	GS   = []byte{0x1D}
+	INIT = []byte{0x1B, 0x40}       // Initialize printer
+	CUT  = []byte{0x1D, 0x56, 0x41} // Cut paper
+	LF   = []byte{0x0A}             // Line feed
+)
+
 func main() {
 	// Create a new USB context
 	ctx := gousb.NewContext()
 	defer ctx.Close()
 
-	// Get a list of all USB devices
-	devices, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
-		// This function is called for each device.
-		// Returning true means the device should be opened.
-		return true
-	})
-
+	// Find the specific device
+	dev, err := ctx.OpenDeviceWithVIDPID(0x0483, 0x070b)
 	if err != nil {
-		log.Fatalf("Error opening devices: %v", err)
+		log.Fatalf("Could not open device: %v", err)
+	}
+	defer dev.Close()
+
+	// Claim the default interface (usually interface 0)
+	intf, done, err := dev.DefaultInterface()
+	if err != nil {
+		log.Fatalf("Error claiming interface: %v", err)
+	}
+	defer done()
+
+	// Find the OUT endpoint
+	epOut, err := intf.OutEndpoint(0x01) // Assuming the first OUT endpoint
+	if err != nil {
+		log.Fatalf("Error finding OUT endpoint: %v", err)
 	}
 
-	// Ensure all devices are closed when we're done
-	defer func() {
-		for _, d := range devices {
-			d.Close()
-		}
-	}()
-
-	// Print information about each device
-	for i, dev := range devices {
-		fmt.Printf("Device %d:\n", i)
-		fmt.Printf("  Bus: %d\n", dev.Desc.Bus)
-		fmt.Printf("  Address: %d\n", dev.Desc.Address)
-		fmt.Printf("  Speed: %s\n", dev.Desc.Speed)
-		fmt.Printf("  Vendor ID: %s\n", dev.Desc.Vendor)
-		fmt.Printf("  Product ID: %s\n", dev.Desc.Product)
-
-		// Get the device configuration
-		cfg, err := dev.Config(1)
-		if err != nil {
-			log.Printf("Error getting config: %v", err)
-			continue
-		}
-
-		// Print information about each interface
-		for _, intf := range cfg.Desc.Interfaces {
-			for _, ifSetting := range intf.AltSettings {
-				fmt.Printf("    Interface %d:\n", ifSetting.Number)
-				fmt.Printf("      Alt Setting: %d\n", ifSetting.Alternate)
-				fmt.Printf("      Class: %d\n", ifSetting.Class)
-				fmt.Printf("      SubClass: %d\n", ifSetting.SubClass)
-				fmt.Printf("      Protocol: %d\n", ifSetting.Protocol)
-			}
-		}
-
-		fmt.Println()
+	// Send initialization command
+	_, err = epOut.Write(INIT)
+	if err != nil {
+		log.Fatalf("Error initializing printer: %v", err)
 	}
+
+	// Print some text
+	text := []byte("Hello, Thermal Printer!\n")
+	_, err = epOut.Write(text)
+	if err != nil {
+		log.Fatalf("Error writing text: %v", err)
+	}
+
+	// Feed a line
+	_, err = epOut.Write(LF)
+	if err != nil {
+		log.Fatalf("Error feeding line: %v", err)
+	}
+
+	// Cut the paper
+	_, err = epOut.Write(CUT)
+	if err != nil {
+		log.Fatalf("Error cutting paper: %v", err)
+	}
+
+	fmt.Println("Print job sent successfully!")
 }
