5a8b84777e7e3e4cc5fade24ae9bcec299a0a4b1
“sokyran” <citramonarchy@gmail.com>
2024-09-02 23:32:28 +0300

    try

--- go/main.go
+++ go/main.go
@@ -55,11 +55,7 @@ func main() {
 	p := escpos.New(epOut)
 
 	p.Bold(true).Size(1, 1).Write("Hello World")
-	p.LineFeed()
 	p.Bold(false).Underline(2).Justify(escpos.JustifyCenter).Write("this is underlined")
-	p.LineFeed()
-
-	p.LineFeed()
 
 	// You need to use either p.Print() or p.PrintAndCut() at the end to send the data to the printer.
 	p.Print()
