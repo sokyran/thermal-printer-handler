6c8525dc7f0d86448c9bd506592e69578320348b
“sokyran” <citramonarchy@gmail.com>
2024-10-13 19:49:56 +0300

    try

--- other-pos.js
+++ other-pos.js
@@ -1,29 +1,6 @@
 import EscPosEncoder from 'esc-pos-encoder';
 import { findByIds } from 'usb';
 
-const encodings = [
-  "cp437", "cp720", "cp737", "cp775", "cp850", "cp851", "cp852", "cp853", "cp855", "cp857",
-  "cp858", "cp860", "cp861", "cp862", "cp863", "cp864", "cp865", "cp866", "cp869", "cp874",
-  "cp922", "cp1098", "cp1118", "cp1119", "cp1125", "cp2001", "cp3001", "cp3002", "cp3011",
-  "cp3012", "cp3021", "cp3041", "cp3840", "cp3841", "cp3843", "cp3844", "cp3845", "cp3846",
-  "cp3847", "cp3848", "iso885915", "iso88592", "iso88597", "rk1048", "windows1250", "windows1251",
-  "windows1252", "windows1253", "windows1254", "windows1255", "windows1256", "windows1257",
-  "windows1258"
-];
-
-// encodings.forEach((encoding) => {
-//   try {
-//     let result = encoder
-//       .codepage(encoding)
-//       .text('інтернаціоналізація')
-//       .encode()
-
-//     console.log(result);
-//   } catch {
-//     // console.log('Error encoding:', encoding);
-//   }
-// });
-
 // Step 1: Find the Printer
 const printerVendorId = 1155; // Replace with your printer's vendor ID
 const printerProductId = 1803; // Replace with your printer's product ID
@@ -57,26 +34,12 @@ const encoder = new EscPosEncoder({
   width: 34,
 });
 
-const text = 'інтернаціоналізація';
-
-let result = encoder.initialize()
-
-for (const encoding of encodings) {
-  try {
-    result = result
-      .codepage(encoding)
-      .text(text)
-
-    console.log('Encoding:', encoding);
-  } catch {
-    console.log('Error encoding:', encoding);
-  }
-}
-
-const final = result.encode();
+const result = encoder
+  .initialize()
+  .image('./image.jpg', 34).encode()
 
 // Step 4: Send the Data to the Printer
-endpoint.transfer(final, (error) => {
+endpoint.transfer(result, (error) => {
   if (error) {
     console.error('Failed to print:', error);
   } else {
